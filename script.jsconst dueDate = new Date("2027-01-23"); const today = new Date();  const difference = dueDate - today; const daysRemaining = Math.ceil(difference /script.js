6e4a5c2353ac4e91fbc9a0ced22815505e7e5ea4
JenniferCasson <cassonjg@gmail.com>
2026-07-27 10:14:03 +0100

    Delete script.jsconst dueDate = new Date("2027-01-23"); const today = new Date();  const difference = dueDate - today; const daysRemaining = Math.ceil(difference  directory

--- script.jsconst dueDate = new Date("2027-01-23"); const today = new Date();  const difference = dueDate - today; const daysRemaining = Math.ceil(difference /script.js
+++
@@ -1,7 +0,0 @@
-const dueDate = new Date("2027-01-23");
-const today = new Date();
-
-const difference = dueDate - today;
-const daysRemaining = Math.ceil(difference / (1000 * 60 * 60 * 24));
-
-document.getElementById("daysRemaining").textContent = daysRemaining;
